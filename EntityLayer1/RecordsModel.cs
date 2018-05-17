4afa8431fdb6bc1e43b16adae297c044a653eda7
Muhammad Anees Qureshi <muhammadanish2011@gmail.com>
2018-05-17 10:34:30 +0530

    Local Connection string for database updated which is present in dev server named as airpollutionService_db

--- EntityLayer1/RecordsModel.cs
+++ EntityLayer1/RecordsModel.cs
@@ -12,13 +12,13 @@ namespace EntityLayer1
         public string country { get; set; }
         public string state { get; set; }
         public string city { get; set; }
-        //public string station { get; set; }
-        //public string last_update { get; set; }
-        //public string pollutant_id { get; set; }
-        //public string pollutant_min { get; set; }
-        //public string pollutant_max { get; set; }
-        //public string pollutant_avg { get; set; }
-        //public string pollutant_unit { get; set; }
+        public string station { get; set; }
+        public string last_update { get; set; }
+        public string pollutant_id { get; set; }
+        public string pollutant_min { get; set; }
+        public string pollutant_max { get; set; }
+        public string pollutant_avg { get; set; }
+        public string pollutant_unit { get; set; }
 
     }
 }
